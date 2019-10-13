ec81298093c1a32f3fd5b5068b1e2164cd05fe2d
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-13 00:33:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.collect do |season, season_values|
+  holiday_hash.each do |season, season_values|
   #  binding.pry
     puts "#{season.to_s.capitalize}:"
           #  #{season_values.to_s.capitalize}"
